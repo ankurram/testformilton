67ac92a804e6df27c88b10831fde5eefaf6026aa
ankur <ankurram5@gmail.com>
2021-07-27 09:28:04 +0530

    remove comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,8 @@ import { HttpServiceService } from './service/http-service.service'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testdeploy';
   photoList: any=[];
   photoListTable :any =[];
-  toggle = true;
   text = 'Compare';
   constructor(
      private http:HttpServiceService
@@ -18,19 +16,15 @@ export class AppComponent {
 
   ngOnInit(){
        this.http.getData().subscribe((response)=>{
-              console.log("this is response",response.slice(0, 10))
               this.photoList = response.slice(0, 3)
        }) 
    }
 
    addInTable(value){
-   //  document.getElementById("btn-"+value.id).innerHTML = document.getElementById("btn-"+value.id).innerHTML == "Remove" ? "Compare" : "Remove";
-    console.log(document.getElementById("btn-"+value.id).innerHTML)
     if((document.getElementById("btn-"+value.id).innerHTML) == 'Compare'){
       document.getElementById("btn-"+value.id).innerHTML = document.getElementById("btn-"+value.id).innerHTML == "Remove" ? "Compare" : "Remove";
        this.photoListTable.push(value)
       } else{
-      console.log("this is remove")
       document.getElementById("btn-"+value.id).innerHTML = document.getElementById("btn-"+value.id).innerHTML == "Remove" ? "Compare" : "Remove";
       var index = this.photoListTable.findIndex(function(o){
          return o.id === value.id;
